1cfb42d9432f00d14239eccc3fa28c8b94df1542
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-08 16:04:18 +0300

    Startup configuration has been completed

--- IdentityServer.AuthServer/Startup.cs
+++ IdentityServer.AuthServer/Startup.cs
@@ -20,13 +20,17 @@ namespace IdentityServer.AuthServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer().AddInMemoryApiResources(Config
+                .GetApiResource())
+                .AddInMemoryApiScopes(Config.GetApiScopes())
+                .AddInMemoryClients(Config.GetClients())
+         
+                .AddDeveloperSigningCredential();
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,14 +40,13 @@ namespace IdentityServer.AuthServer
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseIdentityServer();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
