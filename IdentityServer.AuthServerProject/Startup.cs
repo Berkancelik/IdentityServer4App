b4217e5617fa9160becde6ce30a1bcda294a0336
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-08 12:44:36 +0300

    Fixed

--- IdentityServer.AuthServerProject/Startup.cs
+++ IdentityServer.AuthServerProject/Startup.cs
@@ -20,13 +20,11 @@ namespace IdentityServer.AuthServerProject
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
