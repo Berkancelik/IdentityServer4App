413e154aef12be22c8b447212fa5517945285f4a
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-08 12:43:22 +0300

    Add project files.

---
+++ IdentityServer.AuthServerProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServer.AuthServerProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
