f193a6bd3a94612a08bc55ce4601eaac530246eb
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-28 21:21:17 +0300

    fixed

--- IdentityServer.AuthServer/Config.cs
+++ IdentityServer.AuthServer/Config.cs
@@ -6,18 +6,16 @@ namespace IdentityServer.AuthServer
 {
     public static class Config
     {
+
         public static IEnumerable<ApiResource> GetApiResource()
         {
             return new List<ApiResource>()
             {
-                new ApiResource("resource_api1")
-                {
-                    Scopes={"api1.read","api1.write","api1.update"}
-                },
+                new ApiResource("resource_api1"){
+                    Scopes={ "api1.read","api1.write","api1.update" } },
                 new ApiResource("resource_api2")
                 {
-                    Scopes={"api2.read","api2.write","api2     .update"}
-
+                       Scopes={ "api2.read","api2.write","api2.update" }
                 }
             };
         }
@@ -27,33 +25,33 @@ namespace IdentityServer.AuthServer
             return new List<ApiScope>()
             {
                 new ApiScope("api1.read","API 1 için okuma izni"),
-                new ApiScope("api1.write","API 1 için yazma izni"),
-                new ApiScope("api1.update","API 1 için güncelleme izni"),
-
-                new ApiScope("api1.read","API 2 için okuma izni"),
-                new ApiScope("api1.write","API 2 için yazma izni"),
-                new ApiScope("api1.update","API 2 için güncelleme izni")
+                  new ApiScope("api1.write","API 1 için yazma izni"),
+                    new ApiScope("api1.update","API 1 için güncelleme izni"),
+                       new ApiScope("api2.read","API 2 için okuma izni"),
+                  new ApiScope("api2.write","API 2 için yazma izni"),
+                    new ApiScope("api2.update","API 2 için güncelleme izni"),
             };
         }
 
         public static IEnumerable<Client> GetClients()
         {
-            return new List<Client>() {
-                new Client() {
-                ClientId = "Client1",
-                ClientName= "Client 1  app uygulaması",
-                ClientSecrets=new[]{new Secret("secret".Sha256()) },
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                AllowedScopes={"api1.read","api2.write","api2.update"}
-            },
-              new Client() {
-                ClientId = "Client2",
-                ClientName= "Client 2  app uygulaması",
-                ClientSecrets=new[]{new Secret("secret".Sha256()) },
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                AllowedScopes={"api1.read","api2.write","api2.update"}
-              }
-
+            return new List<Client>(){
+                new Client()
+                {
+                    ClientId = "Client1",
+                    ClientName="Client 1 app uygulaması",
+                   ClientSecrets=new[] {new Secret("secret".Sha256())},
+                   AllowedGrantTypes= GrantTypes.ClientCredentials,
+                   AllowedScopes= {"api1.read"}
+                },
+                 new Client()
+                {
+                    ClientId = "Client2",
+                    ClientName="Client 2 app uygulaması",
+                   ClientSecrets=new[] {new Secret("secret".Sha256())},
+                   AllowedGrantTypes= GrantTypes.ClientCredentials,
+                   AllowedScopes= {"api1.read" ,"api2.write","api2.update"}
+                }
             };
         }
     }
